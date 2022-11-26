546bec76ae372c715f0b5592c142d4711766a119
SAMUETO TOLAMBO <stongambo@gmail.com>
2022-11-26 21:14:05 +0000

    Deploy with ssh

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,11 @@ deploy.sh
 Copy the content and paste it in the Github account SSH.
 
     known_hosts    hosts validated credentia
+git commit -a
+
+PAT
+   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
+    It is use to access and/or manage the GitHub-API resources
+    Use to control access 
+    use to access GitHub account instead of password
+We can generate multiple PAT with different level of access 
